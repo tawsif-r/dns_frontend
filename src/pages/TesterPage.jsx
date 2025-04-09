00d9a08da2880a3682a9fd2d0874a04c9d244fdb
tawsif-r <urrahmantawsif@gmail.com>
2025-04-09 16:53:40 +0600

    check

--- src/pages/TesterPage.jsx
+++ src/pages/TesterPage.jsx
@@ -8,8 +8,8 @@ const TesterPage = () => {
                 <div className='border-2 p-4'>Logo</div>
                 {/* right header*/}
                 <div className='flex flex-row'>
-                    <div className='p-4'>Topics</div>
-                    <div className='p-4'>News</div>
+                    <div className='border-2 border-cyan-200 p-4'>Topics</div>
+                    <div className='border-2 border-emerald-500 p-4'>News</div>
                     <div className='border-2 border-red-200 p-4'>
                         Profile pic
                     </div>
@@ -19,15 +19,25 @@ const TesterPage = () => {
             {/* header end */}
 
             <div className='flex justify-start'>
-                <div className='bg-slate-400 boder-2 border-red-950 m-2'>
+                {/* NAV bar */}
+                <div className='bg-slate-400 boder-2 border-red-950'>
                     <div className='border-2 border-green-200 p-2'>Home</div>
                     <div className='border-2 border-green-200 p-2'>Categories</div>
                     <div className='border-2 border-green-200 p-2'>About</div>
                     <div className='border-2 border-green-200 p-2'>Contact</div>
                 </div>
+                {/*Nav bar end */}
+
+
+
+                {/* Content */}
                 <div className='bg-lime-800 border-2 border-red-400 min-h-screen'>
                     <div className=''><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis illum porro dignissimos assumenda eveniet pariatur natus officia sed eos! Corporis tempora vel reiciendis atque molestiae, fugit corrupti expedita temporibus ipsam.</p></div>
                 </div>
+                {/* Content end */}
+
+
+                {/* side bar */}
                 <div className='bg-lime-800 border-2 border-red-400'>
                     <div className=''>
                         <ul>
@@ -37,6 +47,7 @@ const TesterPage = () => {
                         </ul>
                     </div>
                 </div>
+                {/* side bar end */}
 
 
             </div>
